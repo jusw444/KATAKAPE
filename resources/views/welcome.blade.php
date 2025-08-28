37f152869fd93efccd4ab1f82ed2cde06fa5d7ca
jusw444 <almazanjoshuanoel@gmail.com>
2025-08-28 21:58:57 +0800

    Initial commit for Laravel project with Koppee template

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('content')
+<!-- About Start -->
+    <div class="container-fluid py-5">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-5 pb-4 pb-lg-0">
+                    <img class="img-fluid w-100" src="{{ asset('Koppee-1.0.0/img/about.png') }}" alt="">
+                </div>
+                <div class="col-lg-7">
+                    <h6 class="text-primary text-uppercase font-weight-bold mb-3">About Us</h6>
+                    <h1 class="mb-4 section-title">Welcome to <span class="text-primary">KATAKAPE</span></h1>
+                    <h5 class="mb-3">Coffee is a brewed drink prepared from roasted coffee beans, the seeds of berries from certain Coffea species.</h5>
+@endsection
